879e1f8dd86fd918db8d05237a2bcfc895233d25
Ryunosuke <r.kamata.sys23@morijyobi.ac.jp>
2024-04-02 19:51:46 +0900

    javascript課題

---
+++ exercises/prac_8/board.js
@@ -0,0 +1,17 @@
+"use strict";
+const list = document.getElementById("list");
+
+function prepend() {
+
+    // ニックネームを取得
+    let name = document.querySelector("#name").value;
+    let text = document.querySelector("#text").value;
+    const now = new Date();
+
+    const createElement = document.createElement("p");
+    createElement.innerHTML = `${name}さんが投稿しました。<br>${text}<br>${now.toLocaleString()}`;
+    list.prepend(createElement);
+
+    document.querySelector("#name").value = "";
+    document.querySelector("#text").value = "";
+}
